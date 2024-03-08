b78daf7e03e6cba205eca32567f374350a456212
eno <eno.huang@zyxel.com.tw>
2024-03-08 14:16:09 +0800

    feat(Analytics): add nextjs Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics />
         <SpeedInsights />
         {children}
       </body>
